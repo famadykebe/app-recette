8c4d8e5558401c7b8bd939a63a147ddc3e9881fa
famadykebe <famady932@gmail.com>
2020-10-12 12:01:16 +0200

    update suggestion

--- src/components/suggestion.js
+++ src/components/suggestion.js
@@ -1,5 +1,5 @@
-import React,{useEffect} from 'react'
-import { Link } from 'react-router-dom'
+import React,{useEffect} from 'react';
+import { Link } from 'react-router-dom';
 import { Col } from 'reactstrap';
 import {connect} from 'react-redux';
 import {getRecetteSimilar} from '../store/actions';
